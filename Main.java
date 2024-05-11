557a1ccdfdea3026a18ee37ce9c855221e1bf06b
namek1305 <namek1305@gmail.com>
2024-05-11 18:34:32 +0300

    edit 2 Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.print("Это добавлено в ветке dev");
+        System.out.print("Это добавлено в ветке dev");
     }
 }
